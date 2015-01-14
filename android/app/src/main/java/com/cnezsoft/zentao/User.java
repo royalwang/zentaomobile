b47c82db7b0df75aefcfbf21b6fe8da2de0af88e
Catouse <catouse@gmail.com>
2015-01-14 17:04:11 +0800

    + add UserPreferences.java.  * change user class.

--- android/app/src/main/java/com/cnezsoft/zentao/User.java
+++ android/app/src/main/java/com/cnezsoft/zentao/User.java
@@ -1,5 +1,8 @@
 package com.cnezsoft.zentao;
 
+import org.json.JSONException;
+import org.json.JSONStringer;
+
 import java.util.Date;
 
 /**
@@ -7,10 +10,49 @@ import java.util.Date;
  * Created by Catouse on 2015/1/12.
  */
 public class User {
+
+    public enum Status {
+        Unknown,
+        Offline,
+        Online
+    };
+
     private String account;
     private String address;
     private String passwordMD5;
     private Date lastLoginTime;
+    private Status status = Status.Unknown;
+    private UserPreferences userPreferences;
+    private String identify;
+    private final String ACCOUNT = "ACCOUNT";
+    private final String PASSWORDMD5 = "PASSWORDMD5";
+    private final String ADDRESS = "ADDRESS";
+    private final String LASTLOGINTIME = "LASTLOGINTIME";
+
+    /**
+     * Get current user identify
+     * @return
+     */
+    public String getIdentify()
+    {
+        return (identify != null ? identify : (account + "@" + address));
+    }
+
+    /**
+     * Status getter
+     * @return
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * Status setter
+     * @param status
+     */
+    private void setStatus(Status status) {
+        this.status = status;
+    }
 
     /**
      * Get the last login time
@@ -45,73 +87,201 @@ public class User {
     }
 
     /**
+     * Check and save data after user info changed
+     */
+    private void onChange(boolean saveData)
+    {
+        if(status == Status.Unknown
+                && !Helper.isNullOrEmpty(account)
+                && !Helper.isNullOrEmpty(address)
+                && !Helper.isNullOrEmpty(passwordMD5))
+        {
+            setStatus(Status.Offline);
+        }
+
+        identify = account + "@" + address;
+
+        if(saveData) save();
+    }
+
+    /**
+     * Check and save data after user info changed
+     */
+    private void onChange()
+    {
+        onChange(true);
+    }
+
+    /**
+     * Save data with prefrences
+     */
+    public void save()
+    {
+        if(userPreferences != null)
+        {
+            userPreferences.edit()
+                    .setIdentify(getIdentify())
+                    .putString(ACCOUNT, getAccount())
+                    .putString(ADDRESS, getAddress())
+                    .putString(PASSWORDMD5, getPasswordMD5())
+                    .putLong(LASTLOGINTIME, getLastLoginTime().getTime())
+                    .commit();
+        }
+    }
+
+    /**
      * Account setter
      * @param account
      */
-    public void setAccount(String account) {
+    public void setAccount(String account, boolean saveData) {
         this.account = account;
+        if(saveData) onChange();
+    }
+
+    /**
+     * Account setter
+     * @param account
+     */
+    public void setAccount(String account)
+    {
+        this.setAccount(account, true);
     }
 
     /**
      * Address setter
      * @param address
      */
-    public void setAddress(String address) {
+    public void setAddress(String address, boolean saveData) {
         if(!address.startsWith("http://") && !address.startsWith("https://"))
         {
             address = "http://" + address;
         }
         this.address = address;
+        if(saveData) onChange();
     }
 
     /**
-     * PasswordMD% setter
+     * Address setter
+     * @param address
+     */
+    public void setAddress(String address)
+    {
+        setAddress(address, true);
+    }
+
+    /**
+     * PasswordMD5 setter
      * @param passwordMD5
      */
-    public void setPasswordMD5(String passwordMD5) {
+    public void setPasswordMD5(String passwordMD5, boolean saveData) {
         this.passwordMD5 = passwordMD5;
+        if(saveData) onChange();
+    }
+
+    /**
+     * PasswordMD5 setter
+     * @param passwordMD5
+     */
+    public void setPasswordMD5(String passwordMD5)
+    {
+        setPasswordMD5(passwordMD5, true);
     }
 
     /**
      * 'LastLoginTime' setter
      * @param lastLoginTime
      */
-    public void setLastLoginTime(Date lastLoginTime) {
+    public void setLastLoginTime(Date lastLoginTime, boolean saveData) {
         this.lastLoginTime = lastLoginTime;
+        if(saveData) onChange();
     }
 
     /**
-     * Set 'LastLoginTime' with current time
+     * 'LastLoginTime' setter
+     * @param lastLoginTime
      */
-    public void setLastLoginTime() {
+    public void setLastLoginTime(Date lastLoginTime)
+    {
+        setLastLoginTime(lastLoginTime, true);
+    }
+
+    /**
+     * Turn the user status to online
+     */
+    public void online()
+    {
+        setStatus(Status.Online);
         this.setLastLoginTime(new Date());
     }
 
     /**
-     * Constructor with account, address and passwordMD5
+     * Turn the user status offline
+     */
+    public void offline()
+    {
+        setStatus(Status.Offline);
+    }
+
+    /**
+     * Set account, address and passwordMD5
+     * @param account
+     * @param address
+     * @param passwordMD5
+     * @param save
+     */
+    public void set(String account, String address, String passwordMD5, boolean save) {
+        this.setAccount(account, false);
+        this.setAddress(address, false);
+        this.setPasswordMD5(passwordMD5, false);
+        onChange(save);
+    }
+
+    /**
+     * Set account, address and passwordMD5
      * @param account
      * @param address
      * @param passwordMD5
      */
-    public User(String account, String address, String passwordMD5) {
-        this.account = account;
-        this.address = address;
-        this.passwordMD5 = passwordMD5;
+    public void set(String account, String address, String passwordMD5)
+    {
+        set(account, address, passwordMD5, true);
     }
 
     /**
-     * Constructor with account and address
+     * Constructor with account, address, passwordMD5 and userPreferences
      * @param account
      * @param address
+     * @param passwordMD5
      */
-    public User(String account, String address) {
-        this.account = account;
-        this.address = address;
+    public User(String account, String address, String passwordMD5, UserPreferences userPreferences) {
+        this.userPreferences = userPreferences;
+        this.userPreferences.setIdentify(getAccount());
+        set(account, address, passwordMD5);
     }
 
     /**
-     * Constructor without params
+     * Constructor with userPreferences
      */
-    public User() {
+    public User(UserPreferences userPreferences) {
+        this.userPreferences = userPreferences;
+        set(this.userPreferences.getString(ACCOUNT, null),
+                this.userPreferences.getString(ADDRESS, null),
+                this.userPreferences.getString(PASSWORDMD5, null), false);
+        setLastLoginTime(new Date(this.userPreferences.getLong(LASTLOGINTIME, 0)), false);
+    }
+
+    /**
+     * To JSON string
+     * @return
+     * @throws JSONException
+     */
+    public String toJSONString() throws JSONException {
+        return new JSONStringer().object()
+                .key("account").value(this.account)
+                .key("address").value(this.address)
+                .key("passwordMD5").value(this.passwordMD5)
+                .key("lastLoginTime").value(this.lastLoginTime)
+                .key("status").value(this.status)
+                .endObject().toString();
     }
 }
---
+++ android/app/src/main/java/com/cnezsoft/zentao/UserPreferences.java
@@ -0,0 +1,240 @@
+package com.cnezsoft.zentao;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Set;
+
+/**
+ * Created by Catouse on 2015/1/14.
+ */
+public class UserPreferences {
+    private final String PREFERENCES_NAME = "com.cnezsoft.zentao.userdata";
+    private final String CURRENT_IDENTIFY = "#CURRENT_IDENTIFY";
+    private final String USER_LIST = "#USER_LIST";
+    private SharedPreferences preferences;
+    private String identify;
+    private SharedPreferences.Editor editor;
+
+    /**
+     * Create a Editor if haven't.
+     * @return
+     */
+    public UserPreferences edit() {
+        if(editor == null) editor = preferences.edit();
+        return this;
+    }
+
+    /**
+     * Set a string value in the preferences editor
+     * @param key
+     * @param value
+     * @return
+     */
+    public UserPreferences putString(String key, String value) {
+        editor.putString(identify + "::" + key, value);
+        return this;
+    }
+
+    /**
+     * Set a set of string values in the preferences editor
+     * @param key
+     * @param values
+     * @return
+     */
+    public UserPreferences putStringSet(String key, Set<String> values) {
+        editor.putStringSet(identify + "::" + key, values);
+        return this;
+    }
+
+    /**
+     * Set a int value in the preferences editor
+     * @param key
+     * @param value
+     * @return
+     */
+    public UserPreferences putInt(String key, int value) {
+        editor.putInt(identify + "::" + key, value);
+        return this;
+    }
+
+    /**
+     * Set a long value in the preferences editor
+     * @param key
+     * @param value
+     * @return
+     */
+    public UserPreferences putLong(String key, long value) {
+        editor.putLong(identify + "::" + key, value);
+        return this;
+    }
+
+    /**
+     * Set a float value in the preferences editor
+     * @param key
+     * @param value
+     * @return
+     */
+    public UserPreferences putFloat(String key, float value) {
+        editor.putFloat(identify + "::" + key, value);
+        return this;
+    }
+
+    /**
+     * Set a boolean value in the preferences editor
+     * @param key
+     * @param value
+     * @return
+     */
+    public UserPreferences putBoolean(String key, boolean value) {
+        editor.putBoolean(identify + "::" + key, value);
+        return this;
+    }
+
+    /**
+     * Mark in the editor that a user preference value should be removed
+     * @param key
+     * @return
+     */
+    public UserPreferences remove(String key) {
+        editor.remove(identify + "::" + key);
+        return this;
+    }
+
+    /**
+     * Clear all users's data
+     * @return
+     */
+    public UserPreferences clear() {
+        editor.clear();
+        return this;
+    }
+
+    /**
+     * Commit user preferences changes back from this Editor to the
+     * {@link SharedPreferences} object it is editing.
+     * @return
+     */
+    public UserPreferences commit() {
+        editor.commit();
+        return this;
+    }
+
+    /**
+     * Set the identify, if editor is available then put in the editor
+     * @param identify
+     * @return
+     */
+    public UserPreferences setIdentify(String identify) {
+        this.identify = identify;
+        if(editor != null) editor.putString(CURRENT_IDENTIFY, identify);
+        return this;
+    }
+
+    /**
+     * Get the current identify
+     * @param load
+     * @return
+     */
+    public String getIdentify(boolean load) {
+        if(load)
+        {
+            identify = preferences.getString(CURRENT_IDENTIFY, null);
+        }
+        return identify;
+    }
+
+    /**
+     * Get the current identify quickly
+     * @return
+     */
+    public String getIdentify() {
+        return getIdentify(false);
+    }
+
+    /**
+     * Get all users list as a strings set
+     * @return
+     */
+    public Set<String> getUserList() {
+        return getStringSet(USER_LIST, null);
+    }
+
+    /**
+     * Constructor with application context
+     * @param context
+     */
+    public UserPreferences(Context context) {
+        preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        getIdentify(true);
+    }
+
+    /**
+     * Retrieve a string value from the user preferences.
+     * @param key
+     * @param defValue
+     * @return
+     */
+    public String getString(String key, String defValue) {
+        return preferences.getString(identify + "::" + key, defValue);
+    }
+
+    /**
+     * Retrieve a set of String values from the user preferences.
+     * @param key
+     * @param defValues
+     * @return
+     */
+    public Set<String> getStringSet(String key, Set<String> defValues) {
+        return preferences.getStringSet(identify + "::" + key, defValues);
+    }
+
+    /**
+     * Retrieve a int value from the user preferences.
+     * @param key
+     * @param defValue
+     * @return
+     */
+    public int getInt(String key, int defValue) {
+        return preferences.getInt(identify + "::" + key, defValue);
+    }
+
+    /**
+     * Retrieve a long value from the user preferences.
+     * @param key
+     * @param defValue
+     * @return
+     */
+    public long getLong(String key, long defValue) {
+        return preferences.getLong(identify + "::" + key, defValue);
+    }
+
+    /**
+     * Retrieve a float value from the user preferences.
+     * @param key
+     * @param defValue
+     * @return
+     */
+    public float getFloat(String key, float defValue) {
+        return preferences.getFloat(identify + "::" + key, defValue);
+    }
+
+    /**
+     * Retrieve a boolean value from the user preferences.
+     * @param key
+     * @param defValue
+     * @return
+     */
+    public boolean getBoolean(String key, boolean defValue) {
+        return preferences.getBoolean(identify + "::" + key, defValue);
+    }
+
+    /**
+     * Checks whether the user preferences contains a value.
+     * @param key
+     * @return
+     */
+    public boolean contains(String key) {
+        return preferences.contains(identify + "::" + key);
+    }
+}
