f2cdfd1176d2ecb3d67396e1fe1e8a66495118af
Catouse <catouse@gmail.com>
2015-01-22 11:02:47 +0800

    * add navigation drawer to "MainActivity".

--- android/app/src/main/java/com/cnezsoft/zentao/MainActivity.java
+++ android/app/src/main/java/com/cnezsoft/zentao/MainActivity.java
@@ -1,16 +1,49 @@
 package com.cnezsoft.zentao;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.cnezsoft.zentao.data.DataEntry;
+import com.cnezsoft.zentao.data.EntryType;
+import com.cnezsoft.zentao.data.Todo;
+import com.cnezsoft.zentao.data.TodoDAO;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * Main activity
+ */
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks{
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    /**
+     * Used to store Zentao application context
+     */
     private ZentaoApplication application;
 
     @Override
@@ -18,16 +51,32 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Navigation drawer
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        // Check user status
         application = (ZentaoApplication) getApplicationContext();
         application.checkUserStatus(this);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -50,7 +99,12 @@ public class MainActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == ZentaoApplication.LOGIN_REQUEST)
         {
+            // Show message
             Toast.makeText(application, getText(resultCode == RESULT_OK ? R.string.message_login_success : R.string.message_login_failed), Toast.LENGTH_SHORT).show();
+
+            // Start syncService
+            startService(new Intent(this, ZentaoSyncService.class));
+
 //            new AlertDialog.Builder(this).setTitle(requestCode + "> 登录界面结果:" + resultCode).setMessage(((ZentaoApplication) getApplicationContext()).getUser().toJSONString()).show();
         }
     }
@@ -61,5 +115,101 @@ public class MainActivity extends ActionBarActivity {
      */
     public void openLoginActivity(View view) {
         application.login(this, false);
+    }
+
+    public void showDatabase(View view) {
+        String logKey = "DATABASE TEST";
+        long now = new Date().getTime();
+        long minute = 60*1000;
+        long counter = 0;
+        Set<Todo> todos;
+        Set<DataEntry> entries = new HashSet<>();
+        TodoDAO dao = new TodoDAO(this);
+
+        Log.d(logKey, "******************************");
+        Log.d(logKey, "== show database ==");
+        counter = dao.count(EntryType.Todo);
+        Log.d(logKey, "count: " + counter);
+        for(Todo td: dao.query()) {
+            Log.d(logKey, "todo: " + td.toJSONString());
+        }
+    }
+
+    public void openTodoListActivity(View view) {
+
+    }
+
+    /**
+     * Called when an item in the navigation drawer is selected.
+     *
+     * @param position
+     */
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
     }
 }
