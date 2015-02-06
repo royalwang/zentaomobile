ea62af5e21d5c30784fcf90438f5c2521e807081
Catouse <catouse@gmail.com>
2015-02-06 16:38:51 +0800

    * change settings activity.

--- android/app/src/main/java/com/cnezsoft/zentao/SettingsActivity.java
+++ android/app/src/main/java/com/cnezsoft/zentao/SettingsActivity.java
@@ -1,10 +1,16 @@
 package com.cnezsoft.zentao;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.joanzapata.android.iconify.IconDrawable;
 import com.joanzapata.android.iconify.Iconify;
@@ -13,6 +19,11 @@ public class SettingsActivity extends ZentaoActivity {
 
     private User user;
     private ZentaoApplication application;
+    private TextView textZentaoAddress;
+    private TextView textZentaoUser;
+    private TextView textSyncFreqName;
+    private AlertDialog.Builder dialogBuilder = null;
+    private Switch switchDisplayNotify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,36 @@ public class SettingsActivity extends ZentaoActivity {
 
         application = (ZentaoApplication) this.getApplicationContext();
         user = application.getUser();
+
+        textZentaoAddress = (TextView) findViewById(R.id.text_zentao_address);
+        textZentaoUser = (TextView) findViewById(R.id.text_zentao_user);
+        textSyncFreqName = (TextView) findViewById(R.id.text_sync_freq_name);
+        switchDisplayNotify = (Switch) findViewById(R.id.switch_display_notify);
+        switchDisplayNotify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                user.setNotify(isChecked).save();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshUserInfo();
+    }
+
+    private void refreshUserInfo() {
+        user.load();
+        textZentaoAddress.setText(user.getAddress());
+        textZentaoUser.setText(user.getAccount());
+        textSyncFreqName.setText(user.getSyncFreqName(this));
+        switchDisplayNotify.setChecked(user.isNotify());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
@@ -54,5 +95,31 @@ public class SettingsActivity extends ZentaoActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeFreq(View view) {
+        if(dialogBuilder == null) {
+            dialogBuilder = new AlertDialog.Builder(this);
+            dialogBuilder.setTitle("请选择同步频率");
+            dialogBuilder.setItems(SyncFrequency.getAllItemsText(this), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    user.setSyncFrequency(SyncFrequency.values()[which]).save();
+                    refreshUserInfo();
+                    dialog.dismiss();
+                }
+            });
+            dialogBuilder.create();
+        }
+        dialogBuilder.show();
+    }
+
+    public void openZentao(View view) {
+        ZentaoApplication.openBrowser(this, user.getAddress());
+    }
+
+    public void onUserLogout(View view) {
+        application.logout(this);
+        finish();
     }
 }
