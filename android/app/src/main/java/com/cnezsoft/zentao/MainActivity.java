e88d239337f1dbbf92b84ddf800466dc090ede7f
Catouse <catouse@gmail.com>
2015-02-13 11:16:29 +0800

    * change statusbar color of MainActivity.

--- android/app/src/main/java/com/cnezsoft/zentao/MainActivity.java
+++ android/app/src/main/java/com/cnezsoft/zentao/MainActivity.java
@@ -77,6 +77,8 @@ public class MainActivity extends ZentaoActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setAccentSwatch(MaterialColorSwatch.Blue);
+
         // Check user status
         application = (ZentaoApplication) getApplicationContext();
         user = application.getUser();
