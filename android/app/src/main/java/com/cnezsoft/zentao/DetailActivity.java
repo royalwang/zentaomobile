a6c723373e9d50d0927297efd222ea05b8cacf60
Catouse <catouse@gmail.com>
2015-03-03 13:17:45 +0800

    * change UI of DetailActivity.

--- android/app/src/main/java/com/cnezsoft/zentao/DetailActivity.java
+++ android/app/src/main/java/com/cnezsoft/zentao/DetailActivity.java
@@ -6,9 +6,16 @@ import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.cnezsoft.zentao.colorswatch.MaterialColorName;
@@ -20,6 +27,10 @@ import com.cnezsoft.zentao.data.DataEntryFactory;
 import com.cnezsoft.zentao.data.EntryType;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 public class DetailActivity extends ZentaoActivity {
     public static final String ARG_ENTRY_TYPE = "com.cnezsoft.zentao.ENTRY_TYPE";
@@ -28,6 +39,10 @@ public class DetailActivity extends ZentaoActivity {
     protected int entryId;
     protected EntryType entryType;
     protected ZentaoApplication application;
+
+    private ListView metaList;
+    private ArrayList<HashMap<String, Object>> metaData;
+    private MetaListAdapter metaAdapter;
 
     protected TextView iconView;
     protected TextView iconTextView;
@@ -67,10 +82,51 @@ public class DetailActivity extends ZentaoActivity {
     }
 
     private void onDataLoad(DataEntry dataEntry) {
+        metaData.clear();
         display(dataEntry);
+        metaAdapter.notifyDataSetChanged();
+        Helper.setListViewHeightBasedOnChildren(metaList);
+
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+        scrollView.post(new Runnable() { public void run() { scrollView.fullScroll(View.FOCUS_UP); } });
     }
 
     protected void display(DataEntry dataEntry) {
+    }
+
+    protected void displayMeta(HashMap<String, Object> meta) {
+        metaData.add(meta);
+    }
+
+    protected void displayMeta(String name, Object content, Object icon, Object iconBack, boolean divider) {
+        HashMap<String, Object> meta = new HashMap<>();
+        meta.put("name", name);
+        meta.put("content", content);
+        meta.put("icon", icon);
+        if(icon!=null && iconBack == null) {
+            iconBack = "{fa-circle-thin}";
+        }
+        meta.put("iconBack", iconBack);
+        if(divider == false) {
+            meta.put("divider", false);
+        }
+        metaData.add(meta);
+    }
+
+    protected void displayMeta(String name, Object content, boolean divider) {
+        displayMeta(name, content, "", "", divider);
+    }
+
+    protected void displayMeta(String name, Object content, Object icon, Object iconBack) {
+        displayMeta(name, content, icon, iconBack, true);
+    }
+
+    protected void displayMeta(String name, Object content, Object icon) {
+        displayMeta(name, content, icon, null);
+    }
+
+    protected void displayMeta(String name, Object content) {
+        displayMeta(name, content, null);
     }
 
     protected void displayOnTextview(TextView textview, String title) {
@@ -189,6 +245,12 @@ public class DetailActivity extends ZentaoActivity {
         }
 
         setContentView(R.layout.activity_detail);
+
+        metaList = findListViewById(R.id.list_meta);
+        metaData = new ArrayList<>();
+        metaAdapter = new MetaListAdapter(this, metaData);
+        metaAdapter.setClickable(false);
+        metaList.setAdapter(metaAdapter);
 
         iconView = (TextView) findViewById(R.id.icon);
         iconTextView = (TextView) findViewById(R.id.icon_text);
