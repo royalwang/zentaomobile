b3fdea44956c9ed164e046e7e14535fd66d307ec
Catouse <catouse@gmail.com>
2015-03-13 16:48:10 +0800

    + add UIColorExtension.

---
+++ iOS/zentao/zentao/UIColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColorExtension.swift
+//  zentao
+//
+//  Created by Sun Hao on 15/3/13.
+//  Copyright (c) 2015年 cnezsoft.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience public init(hex: Int, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
+        let blue = CGFloat((hex & 0xFF)) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+}
