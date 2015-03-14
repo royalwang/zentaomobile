76b09899843367726cba4821993dbe3507e18dfc
Catouse <catouse@gmail.com>
2015-03-14 17:10:22 +0800

    * refactor HTTP helpers.

--- iOS/zentao/zentao/HTTP.swift
+++ iOS/zentao/zentao/HTTP.swift
@@ -14,9 +14,9 @@ struct HTTP {
     static var debug = 1
    
     static func get(url: String, success:((HTTPResponse) -> Void)!,
-        failure:((NSError, HTTPResponse?) -> Void)!, gzip: Bool = withGZip) {
+        failure:((NSError, HTTPResponse?) -> Void)!) {
         let request = HTTPTask()
-        if gzip {
+        if HTTP.withGZip {
             request.requestSerializer = HTTPRequestSerializer()
             request.requestSerializer.headers["Accept-Encoding"] = "gzip"
         }
@@ -36,28 +36,28 @@ struct HTTP {
     }
     
     static func get(url: String, success:((String?) -> Void)!,
-        failure:((NSError, HTTPResponse?) -> Void)!, gzip: Bool = true) {
+        failure:((NSError, HTTPResponse?) -> Void)!) {
         get(url, success: {(response: HTTPResponse) -> Void in
             success(response.text())
-            }, failure: failure, gzip: gzip)
+            }, failure: failure)
     }
     
     static func get(url: String, complete:((String?) -> Void)!, gzip: Bool = true) {
         get(url, success: complete, failure: {(error: NSError, response: HTTPResponse?) -> Void in
             complete(nil)
-        }, gzip: gzip)
+        })
     }
     
     static func get(url: String, success:((JSON?) -> Void)!,
-        failure:((NSError, HTTPResponse?) -> Void)!, gzip: Bool = true) {
+        failure:((NSError, HTTPResponse?) -> Void)!) {
         get(url, success: {(response: HTTPResponse) -> Void in
             success(response.responseObject != nil ? JSON(data: response.responseObject as NSData, options: NSJSONReadingOptions.AllowFragments, error: nil) : nil)
-        }, failure: failure, gzip: gzip)
+        }, failure: failure)
     }
     
-    static func get(url: String, complete: ((JSON?) -> Void)!, gzip: Bool = true) {
+    static func get(url: String, complete: ((JSON?) -> Void)!) {
         get(url, success: complete, failure: {(error: NSError, response: HTTPResponse?) -> Void in
             complete(nil)
-        }, gzip: gzip)
+        })
     }
 }
