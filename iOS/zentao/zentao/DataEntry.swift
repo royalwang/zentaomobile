bcc23f66001e1d00d3d6f47b2a1a660fb9f94748
Catouse <catouse@gmail.com>
2015-03-16 18:18:05 +0800

    + add DataEntry.

---
+++ iOS/zentao/zentao/DataEntry.swift
@@ -0,0 +1,14 @@
+//
+//  Entry.swift
+//  zentao
+//
+//  Created by Sun Hao on 15/3/16.
+//  Copyright (c) 2015年 cnezsoft.com. All rights reserved.
+//
+
+import Foundation
+
+protocol DataEntry {
+    
+    var entryType: EntryType {get}
+}
