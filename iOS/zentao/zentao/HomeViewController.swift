bc41e8747e613f5402a285726c2b3b6674d25bcf
Catouse <catouse@gmail.com>
2015-03-23 15:04:00 +0800

    * change Home UI.

--- iOS/zentao/zentao/HomeViewController.swift
+++ iOS/zentao/zentao/HomeViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class HomeViewController: ZentaoViewController {
 
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var helloUserLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.accentSwatch = MaterialColor.Blue
         self.showNavigationBarShadow = false
+        
+        sayHelloToUser()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +31,27 @@ class HomeViewController: ZentaoViewController {
     
     override func onLoginSuccess() {
         println("login success message!")
+        sayHelloToUser()
+    }
+    
+    func sayHelloToUser() {
+        if let user = app.user {
+            companyLabel.text = user.company
+
+            let hour = NSDate().hour()
+            var hello: String
+            if hour < 11 {
+                hello = "上午好"
+            } else if hour < 14 {
+                hello = "中午好"
+            } else if hour < 18 {
+                hello = "下午好"
+            } else {
+                hello = "晚上好"
+            }
+            
+            helloUserLabel.text = "\(hello)，\(user.displayName)"
+        }
     }
     
 
