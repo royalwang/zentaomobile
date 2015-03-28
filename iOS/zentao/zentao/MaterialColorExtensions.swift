6bbf1e3876f28c3832ba23a79f6c41bd0e54cdf6
Catouse <catouse@gmail.com>
2015-03-28 10:07:23 +0800

    * add static method to get PriAccentSwatch to MaterialColor.

---
+++ iOS/zentao/zentao/MaterialColorExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  MaterialColorExtensions.swift
+//  zentao
+//
+//  Created by Sun Hao on 15/3/27.
+//  Copyright (c) 2015年 cnezsoft.com. All rights reserved.
+//
+
+import Foundation
+
+extension MaterialColor {
+    
+    static let PriAccentSwatch: [MaterialColor.Swatch] = [
+        Grey,
+        Red,
+        DeepOrange,
+        Purple,
+        Blue,
+        Indigo,
+        Green
+    ]
+}
